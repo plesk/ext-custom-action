bcd08805ddf9284ceded16806dbdd717d608f561
Nikolai Efimov <nefimov@plesk.com>
2018-03-02 16:02:36 +0700

    Add DS for path from sdk

--- plib/library/EventListener.php
+++ plib/library/EventListener.php
@@ -5,14 +5,14 @@ class Modules_CustomAction_EventListener implements EventListener
     public function handleEvent($objectType, $objectId, $action, $oldValue, $newValue)
     {
         if ($action == 'ext_custom-action_test_invoice_created') {
-            $filePath = $this->getPleskTempPath();
+            $filePath = $this->getPleskTempPath() . DIRECTORY_SEPARATOR . 'ext-custom-action.log';
             $data = [
                 $objectId,
                 $objectType,
                 $oldValue,
                 $newValue,
             ];
-            file_put_contents($filePath . 'ext-custom-action.log', json_encode($data));
+            file_put_contents($filePath, json_encode($data));
         }
         pm_Log::info("Event '{$action}' is successfully handled.");
     }
@@ -24,7 +24,7 @@ class Modules_CustomAction_EventListener implements EventListener
         }
 
         return pm_ProductInfo::getPlatform() == pm_ProductInfo::PLATFORM_WINDOWS ?
-            'C:\\Program Files (x86)\\Plesk\\PrivateTemp\\' : '/usr/local/psa/tmp/';
+            'C:\\Program Files (x86)\\Plesk\\PrivateTemp' : '/usr/local/psa/tmp';
     }
 
 }
